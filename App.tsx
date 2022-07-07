f062932f96e41c2fbaa4dbc19b4af8194eb53902
amhed <aherrera@gmail.com>
2022-07-07 12:58:48 -0500

    Correclty configure vector icons

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { NativeBaseProvider, Box } from 'native-base';
+import { NativeBaseProvider, Text, Box, Icon } from 'native-base';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Box>Hello world</Box>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        <Text>Open up App.js to start working on your app!</Text>
+        <Icon as={FontAwesome} name="user" />
+      </Box>
     </NativeBaseProvider>
   );
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+Next tasks
+
+- [ ] Install icon dependencies for Android/Gradle (https://github.com/oblador/react-native-vector-icons#installation)
+- [ ] Implement xcodegen
