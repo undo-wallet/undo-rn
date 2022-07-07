f38f177b2b11c85413c23afc1d8b7a9d0d791453
amhed <aherrera@gmail.com>
2022-07-07 09:21:23 -0500

    Setting up more dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@native-base/icons': '@native-base/icons/lib',
+        },
+      },
+    ],
+  ],
 };
